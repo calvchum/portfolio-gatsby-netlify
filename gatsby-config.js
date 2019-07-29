e0ce97a5f565feec9154ed1235ca61e2586db78a
Calvin Cheung <calvchum@gmail.com>
2019-07-29 23:23:39 +0800

    added logo into images folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `CBC`,
+    description: `a place to store creations`,
+    author: `@calvchum`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
