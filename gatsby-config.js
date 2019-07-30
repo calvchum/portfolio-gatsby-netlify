d0b71f8f43024206d24d3abc0122ed5995610de6
Calvin Cheung <calvchum@gmail.com>
2019-07-30 12:09:21 +0800

    added some hero text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `CBC`,
+    title: `react developer`,
     description: `a place to store creations`,
     author: `@calvchum`,
   },
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,25 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "styled-components"
+import { colors, SubheaderText } from "../utilities"
+
+const AccentText = styled(SubheaderText)`
+  color: ${colors.primary};
+`
+const TextWrapper = styled.div`
+  justify-content: inline;
+`
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Calvin B Cheung" />
+    <TextWrapper>
+      <SubheaderText>I'm Calvin B. Cheung, a </SubheaderText>
+      <AccentText>react developer</AccentText>{" "}
+      <SubheaderText>and</SubheaderText> <AccentText>entrepeneur</AccentText>
+      <SubheaderText>from Melbourne</SubheaderText>
+    </TextWrapper>
   </Layout>
 )
 
