2096ee22c23de8f774ab82472252281fa1e58ed5
Calvin Cheung <calvchum@gmail.com>
2019-08-20 18:31:18 +0800

    added a markdown page and template and node as per tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -15,6 +14,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
---
+++ src/markdown-pages/post-1.md
@@ -0,0 +1,6 @@
+---
+path: "/blog/my-pursuit"
+date: "2019-20-08"
+title: "This is my story"
+---
+
